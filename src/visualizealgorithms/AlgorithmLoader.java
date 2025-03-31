2b83f3339fa3eb0bb9b40d57e6459cac4d22906e
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-31 12:12:05 +0200

    Disable algorithms in loader

--- src/visualizealgorithms/AlgorithmLoader.java
+++ src/visualizealgorithms/AlgorithmLoader.java
@@ -2,6 +2,11 @@ package visualizealgorithms;
 
 //Project imports
 import visualizealgorithms.bll.algorithm.IAlgorithm;
+import visualizealgorithms.bll.algorithm.sorting.BubbleSort;
+import visualizealgorithms.bll.algorithm.sorting.InsertionSort;
+import visualizealgorithms.bll.algorithm.sorting.QuickSort;
+import visualizealgorithms.bll.algorithm.sorting.SelectionSort;
+
 
 //Java imports
 import java.util.ArrayList;
@@ -19,7 +24,10 @@ public class AlgorithmLoader {
         inputs = "1000;2000;4000;8000;16000;32000;64000;";
 
         //Add implemented/selected algorithms here..
-
+        algorithms.add(new BubbleSort());
+        //algorithms.add(new SelectionSort());
+        //algorithms.add(new InsertionSort());
+        //algorithms.add(new QuickSort());
 
         //more algorithms here :)
     }
